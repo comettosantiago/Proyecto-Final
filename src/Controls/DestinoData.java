255464b0520561374d21f552703da0cadfafb5be
comettosantiago <90621053+comettosantiago@users.noreply.github.com>
2021-10-28 21:27:11 -0300

    Version 1.0.3
    Creada clase DestinoData

--- src/Controls/DestinoData.java
+++ src/Controls/DestinoData.java
@@ -5,10 +5,48 @@
  */
 package Controls;
 
+import Models.Destino;
+import java.sql.Connection;
+import java.util.List;
+
 /**
  *
  * @author Santiago Cometto
  */
 public class DestinoData {
+    //methods
+    private Connection con;
+
+    //constructor
+    public DestinoData(Conexion conexionDestino) {
+        this.con = conexionDestino.conectar();
+    }
+    
+    //methods
+    public void agregarDestino(Destino d){
+        
+    }
+    
+    public void eliminarDestino (int idDestino){
+        
+    }
+    
+    public void editarDestino (Destino d){
+        
+    }
+    
+    public Destino buscarDestino(int idDestino){
+        
+        return null;
+    }
+    
+    public List<Destino> listarDestinos(){
+        
+        return null;
+    }
+    
+    
+    
+    
     
 }
